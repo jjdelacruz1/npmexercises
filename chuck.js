3b17e651d1502b5dccc8f8b08ab88b1309e02c5b
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-09-12 21:22:26 -0500

    First commit

---
+++ chuck.js
@@ -0,0 +1,13 @@
+const Chuck  = require('chucknorris-io'),
+      client = new Chuck();
+var dogs = require('dogs');
+var asciiCats = require('ascii-cats');
+ 
+// Retrieve a random chuck joke
+client.getRandomJoke().then(function (response) {
+    // do stuff here
+    console.log(response.value)
+});
+
+console.log(dogs.snoopy())
+console.log(asciiCats())
